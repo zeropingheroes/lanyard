3dc5721c7ac3e6bf3876cf1d63d76f6ca3100c2b
ilumos <ilumos@gmail.com>
2017-10-21 20:55:09 +0100

    Adding authorisation check for admin menu

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -28,11 +28,13 @@
                 @guest
                     <li><a href="{{ route('login') }}">@lang('title.login')</a></li>
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="">
-                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> @lang('title.admin')
-                        </a>
-                    </li>
+                    @if (Auth::user()->hasRole('super admin', 'admin'))
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="">
+                                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> @lang('title.admin')
+                            </a>
+                        </li>
+                    @endif
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->username }} <span class="caret"></span>
