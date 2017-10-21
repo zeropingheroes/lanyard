3a4e530dea3f4f779ecd9d8ecbc3e40088959abf
ilumos <ilumos@gmail.com>
2017-10-21 13:14:54 +0100

    Adding footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -78,6 +78,11 @@
             @yield('content')
         </div>
     </div>
+    <div id="footer">
+        <div class="container text-center">
+            <p class="text-muted credit"><a href="https://github.com/zeropingheroes/lanyard" target="_blank">LANyard</a> is © <a href="http://www.zeropingheroes.co.uk/" target="_blank">Zero Ping Heroes Ltd</a> and licensed under <a href="https://github.com/zeropingheroes/lanyard/blob/master/LICENSE.txt" target="_blank">AGPLv3</a></p>
+        </div>
+    </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
