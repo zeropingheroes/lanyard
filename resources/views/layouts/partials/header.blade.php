e2bc7825db8d31c66b43bb9fcd9ec19b4dc7f710
ilumos <ilumos@gmail.com>
2017-10-22 22:03:26 +0100

    Adding eonasdan-bootstrap-datetimepicker

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-datetimepicker.css') }}" rel="stylesheet">
 </head>
 <body>
 <!-- Scripts -->
